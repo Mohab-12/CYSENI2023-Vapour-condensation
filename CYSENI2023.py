a61bd01932ecf5f70d441926f73805e7b545600a
Mohab <82895440+Mohab-12@users.noreply.github.com>
2023-05-09 09:06:05 +0300

    Add files via upload

---
+++ CYSENI2023.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# import matplotlib.animation as animation
+# from matplotlib.animation import FuncAnimation, writers
+
+
+# In[2]:
+
+
+#sns.set_theme(style='whitegrid', palette='pastel')
+plt.style.use('ggplot')
+
+
+# In[3]:
+
+
+df=pd.read_excel(r"Horizontalus ruozas-Eksperimetu suvestine.xlsb.xlsx", sheet_name='Sheet3')
+
+
+# In[4]:
+
+
+df.drop(columns='Heat lost ', inplace=True)
+#df.dropna(inplace=True)
+
+
+# In[5]:
+
+
+df['Mixture tin, oC']=df['Mixture tin, oC'].astype('float')
+
+
+# In[6]:
+
+
+# df.columns
+
+
+# In[7]:
+
+
+a=df.loc[df['Re']==5000][['First_Cond', 'Second_Cond', 'Third_Cond', 'Fourth_Cond',
+       'Fifth_Cond', 'Sixth_Cond', 'Seventh_Cond', 'Eighth_Cond','Mixture tin, oC']]
+c=df.loc[df['Re']==5000][[ 'First_heat_Coef', 'Second_heat_Coef', 'Third_heat_Coef',
+       'Fourth_heat_Coef', 'Fifth_heat_Coef', 'Sixth_heat_Coef',
+       'Seventh_heat_Coef', 'Eighth_heat_Coef','Mixture tin, oC']]
+e=df.loc[df['Re']==5000][['First_Nusselt', 'Second_Nusselt', 'Third_Nusselt', 'Fourth_Nusselt',
+       'Fifth_Nusselt', 'Sixth_Nusselt', 'Seventh_Nusselt', 'Eighth_Nusselt','Mixture tin, oC']]
+
+
+# In[8]:
+
+
+Nusselt = e.T
+Heat_transfer_coefficient = c.T
+Rate_of_Condensation = a.T
+Rate_of_Condensation 
+
+
+# In[9]:
+
+
+import streamlit as st
+# df = px.data.gapminder()
+
+st.write("""
+            # Plotting parameters against temperature at `Re = 5000` and `mass fraction  = 20%`
+            """)
+
+
+# In[10]:
+
+
+Nusselt.iloc[-1:].values.ravel()
+
+
+# In[11]:
+
+
+import plotly.express as px
+import streamlit as st
+# df = px.data.gapminder()
+
+T1 = st.sidebar.selectbox(label='Choose a parameter to plot', options=['Nusselt',
+                                                                       'Heat transfer coefficient',
+                                                                       'Rate of Condensation'])
+if T1=='Nusselt':
+    T1=Nusselt
+    col='Nusselt'
+    T2 = st.sidebar.selectbox(label='Choose your Temperature', options=T1.iloc[-1:].values.ravel())
+    column_name = T1.columns[T1.eq(T2).any()][0]
+    fig = px.line(x = T1.index[:-1], y=T1.iloc[:-1, column_name])
+    fig.update_layout(
+            xaxis_title=col,
+            yaxis_title="Temperature")
+    st.write(fig)
+elif T1=='Heat transfer coefficient':
+    T1=Heat_transfer_coefficient
+    col='Heat transfer coefficient'
+    T2 = st.sidebar.selectbox(label='Choose your Temperature', options=T1.iloc[-1:].values.ravel())
+
+    column_name = T1.columns[T1.eq(T2).any()][0]
+    fig = px.line(x = T1.index[:-1], y=T1.iloc[:-1, column_name])
+    fig.update_layout(
+            xaxis_title=col,
+            yaxis_title="Temperature")
+    st.write(fig)
+else: 
+    T1=Rate_of_Condensation
+    col='Rate of condensation'
+    T2 = st.sidebar.selectbox(label='Choose your Temperature', options=T1.iloc[-1:].values.ravel())
+    column_name = T1.columns[T1.eq(T2).any()][0]
+    fig = px.bar(x = T1.index[:-1], y=T1.iloc[:-1, column_name])
+    fig.update_layout(
+        xaxis_title=col,
+        yaxis_title="Temperature")
+    st.write(fig)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
